3a6a0c6ac178d16d48815e8730b31ca845979339
Margret `Pax` Williams <pax.margret@gmail.com>
2020-05-21 06:41:19 -0700

    :construction: Add a text and textfield widget

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: Text(someString),
+            title: MyText(),
           ),
           body: Level1(),
         ),
@@ -34,8 +34,11 @@ class Level1 extends StatelessWidget {
 class Level2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Level3(),
+    return Column(
+      children: <Widget>[
+        MyTextField(),
+        Level3(),
+      ],
     );
   }
 }
@@ -46,5 +49,19 @@ class Level3 extends StatelessWidget {
     return Container(
       child: Text(Provider.of<String>(context)),
     );
+  }
+}
+
+class MyText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Needs data');
+  }
+}
+
+class MyTextField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextField();
   }
 }
